2aee358d218adbc5c9c33620a276230eb95ca243
Paul Richard <Paul Richard>
2017-08-10 21:11:07 -0700

    main display now works with sequelize.

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,25 @@
 DROP DATABASE IF EXISTS Questions_db;
 
 CREATE DATABASE Questions_db;
-USE Questions_db
+USE Questions_db;
+
+id INT AUTO_INCREMENT NOT NULL,
+question_text VARCHAR(225),
+choiceOne VARCHAR(225),
+choiceTwo VARCHAR(225),
+choiceThree VARCHAR(225),
+choiceFour VARCHAR(225),
+questionFive VARCHAR(225),
+question_category VARCHAR(225),
+user_score INT NOT NULL,
+disclaimer VARCHAR(225),
+createdAt TIMESTAMP NOT NULL,
+PRIMARY KEY (id)
+);
+
+USE ???;
 
 
 SELECT * FROM questions;
+
+ALTER TABLE questions MODIFY COLUMN createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- public/js/battlefeud.js
+++ public/js/battlefeud.js
@@ -1,145 +1,188 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBBo2iJfTMTmZk2QVZun4OsVTUQGfkB5Ks",
-    authDomain: "battlefeud.firebaseapp.com",
-    databaseURL: "https://battlefeud.firebaseio.com",
-    projectId: "battlefeud",
-    storageBucket: "battlefeud.appspot.com",
-    messagingSenderId: "351121516171"
-  };
-  firebase.initializeApp(config);
+// console.log(questionArr);
+// var database = firebase.database();
 
-var database = firebase.database();
-// var pushFolder = database.child("pushedResponse"); -- I would eventually like the data pushes and game scores to be stored in different folders.
-var randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
-var strikeCount = 0;
+// var randomQuestion = quest[Math.floor(Math.random() * quest.length)];
+// var strikeCount = 0;
+var questions;
+var questID;
+var questionArr;
+var strikeCount;
 
-// Function that displays the question information on the main screen and sets everything to unanswered:
-function setQuestion(question) {
-	$("#qDisplay").html("<h1><strong>" + question.title + "</strong></h1>");
-	$("#display1").html("<h2 class='unanswered' id='space1'>" + question.answerOne + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
-	$("#display2").html("<h2 class='unanswered' id='space2'>" + question.answerTwo + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
-	$("#display3").html("<h2 class='unanswered' id='space3'>" + question.answerThree + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
-	$("#display4").html("<h2 class='unanswered' id='space4'>" + question.answerFour + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
-	$("#display5").html("<h2 class='unanswered' id='space5'>" + question.answerFive + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
-	$("#questionNumber").text("Question #" + question.number);
-	$("#strikeDisplay").html("");
-	strikeCount = 0;
+function getAllQuests(category) {
+	$.get("/api/questions", function(data) {
+		questionArr = data;
+		// console.log(questionArr);
+		// return questionArr;
+	}).then(function() {
+		console.log(questionArr);
+	});
 }
 
-//displays a question on load:
-setQuestion(randomQuestion);
+getAllQuests();
 
-	// Reruns setQuestion to display a new question when "Random Question" button is pressed:
-	$("#randomQuestion").on("click", function(){
-		setQuestion(randomQuestion);
-		randomQuestion = questionBank[Math.floor(Math.random() * questionBank.length)];
-	});
+// console.log(questionArr);
 
-	//Callback function that runs when a number is entered in the "Question Number" box and "Go" is pressed:
+// getAllQuests();
+
+// console.log(questionArr);
+
+// console.log(questionArr);
+
+function getQuestID(category) {
+    $.get("/api/questions/2", function(data) {
+      // console.log("questions", data[0]);
+	  questions = data[0];
+	  console.log(questions);
+	  setQuestion(questions);
+      });
+  	}
+// getQuestID();  	
+
+	// Function that displays the question information on the main screen and sets everything to unanswered:
+	function setQuestion(questions) {
+		$("#qDisplay").html("<h1><strong>" + questions.question_text + "</strong></h1>");
+		$("#display1").html("<h2 class='unanswered' id='space1'>" + questions.choiceOne + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+		$("#display2").html("<h2 class='unanswered' id='space2'>" + questions.choiceTwo + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+		$("#display3").html("<h2 class='unanswered' id='space3'>" + questions.choiceThree + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+		$("#display4").html("<h2 class='unanswered' id='space4'>" + questions.choiceFour + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+		$("#display5").html("<h2 class='unanswered' id='space5'>" + questions.choiceFive + "</h2>").removeClass("btn-warning").addClass("btn-primary").val("");
+		$("#questionNumber").text("Question #" + questions.id);
+		$("#strikeDisplay").html("");
+		strikeCount = 0;
+	}
+
 	$("#submit").on("click", function() {
-		let selected = $("#qNumber").val();
-		setQuestion(questionBank[selected - 1]);
-	});
-
-	//Will also run if 'Enter' is pressed in the text box:
-	$('#qNumber').bind("enterKey",function(){
-		let selected = $("#qNumber").val();
-		setQuestion(questionBank[selected - 1]);
-	});
-	$('#qNumber').keyup(function(e){
-		if(e.keyCode == 13) {
-		$(this).trigger("enterKey");
-		}
-	});
-
-	// Passes through the clicked div, applies the appropriate class and checks which answer number to reveal
-	function answerClick(answer){
-		$(answer).removeClass("btn-primary").addClass("btn-warning");
-		if(answer.id === "display1"){
-			$("#space1").removeClass("unanswered");
-			audioCorrect(answer);
-		} else if(answer.id === "display2"){
-			$("#space2").removeClass("unanswered");
-			audioCorrect(answer);
-		} else if(answer.id === "display3"){
-			$("#space3").removeClass("unanswered");
-			audioCorrect(answer);
-		} else if(answer.id === "display4"){
-			$("#space4").removeClass("unanswered");
-			audioCorrect(answer);
-		} else if(answer.id === "display5"){
-			$("#space5").removeClass("unanswered");
-			audioCorrect(answer);
-		} else {
-		// alert("uh oh, something went wrong. Please try again.");
-		}
-	};
-		// Play the chime sound unless the button has already been clicked:
-	function audioCorrect(clicked) {
-		let correctSound = document.getElementById("correctAudio");
-		correctSound.currentTime = 0;
-		console.log("value of clicked div:" + clicked.value)
-		if(clicked.value === ""){
-			correctSound.play();
-			$(clicked).val("clicked");
-		}
-	}
-
-	// Displays the text, plays the chime sound, and changes the color of the answer when clicked. Also pushes response to firebase if not previously clicked:
-	$("#display1, #display2, #display3, #display4, #display5").on("click", function(){
-		if(this.value != "clicked"){
-			firebaseSend(this);
-		}
-		answerClick(this);
-	});
-
-	// Will perform count answer as clicked if corresponding number on keyboard is pressed:
-	document.addEventListener("keydown", function(e){
-		let wrongSound = document.getElementById("wrongAudio");
-		wrongSound.currentTime = 0;
-		if (e.keyCode === 49 || e.keyCode === 97) {
-			answerClick(display1);
-		} else if (e.keyCode === 50 || e.keyCode ===  98) {
-			answerClick(display2);
-		} else if (e.keyCode === 51 || e.keyCode === 99) {
-			answerClick(display3);
-		} else if (e.keyCode === 52 || e.keyCode === 100) {
-			answerClick(display4);
-		} else if (e.keyCode === 53 || e.keyCode === 101) {
-			answerClick(display5);
-		} else if (e.keyCode === 32) {
-			strikeCount++;
-			if (strikeCount < 4) {
-			$("#strikeDisplay").append("<h2 id='strike'>X</h2>");
-			wrongSound.play();
-			}
-		}
-	});
-
-	// Function that sends the clicked button's info (ID) to Firebase:
-	function firebaseSend(clicked){
-		database.ref().push({
-			clicked: clicked.id
+		questID = 2;
+		$.get("/api/questions/" + questID, function(data) {
+		// console.log("questions", data[0]);
+		questions = data[0];
+		console.log(questions);
+		setQuestion(questions);
 		});
-	};
-
-	// GAME HOST SCREEN:
-
-	function questionDisplay() {
-		//Runs through the length of the array, and injects this HTML and information for each question:
-		for (let i = 0; i <= questionBank.length; i++) {
-			console.log(questionBank[i].title);
-			$("#displayZone").append('<div class="panel panel-primary"><div class="panel-heading text-center"><h4><strong>' + questionBank[i].title + '</strong></h4></div><div class="panel panel-body"><button class="btn btn-primary btn-block">' +questionBank[i].answerOne + '</button><button class="btn btn-primary btn-block">' +questionBank[i].answerTwo + '</button><button class="btn btn-primary btn-block">' +questionBank[i].answerThree + '</button><button class="btn btn-primary btn-block">' + questionBank[i].answerFour + '</button><button class="btn btn-primary btn-block">' + questionBank[i].answerFive + '</button><h4>Question Number: ' + questionBank[i].number + '</h4><h4>Disclaiming information: ' +questionBank[i].disclaimers + '</div></div>');
-		}
-	}
-
-	questionDisplay();
-
-	database.ref().on("child_added", function(snapshot) {
-		console.log(snapshot.val().clicked);
-		answerClick(snapshot.val().clicked);
 	})
-});
+
+
+			//displays a question on load:
+			// setQuestion(questID);
+
+				// Reruns setQuestion to display a new question when "Random Question" button is pressed:
+				$("#randomQuestion").on("click", function(){
+					questID = questionArr[Math.floor(Math.random() * questionArr.length)];
+					setQuestion(questID);
+					console.log(questID);
+				});
+
+				//Callback function that runs when a number is entered in the "Question Number" box and "Go" is pressed:
+				$("#submit").on("click", function() {
+					questID = $("#qNumber").val();
+					setQuestion(quest[selected - 1]);
+				});
+
+				//Will also run if 'Enter' is pressed in the text box:
+				$('#qNumber').bind("enterKey",function(){
+					let selected = $("#qNumber").val();
+					setQuestion(quest[selected - 1]);
+				});
+				$('#qNumber').keyup(function(e){
+					if(e.keyCode == 13) {
+					$(this).trigger("enterKey");
+					}
+				});
+
+				// Passes through the clicked div, applies the appropriate class and checks which answer number to reveal
+				function answerClick(answer){
+					$(answer).removeClass("btn-primary").addClass("btn-warning");
+					if(answer.id === "display1"){
+						$("#space1").removeClass("unanswered");
+						audioCorrect(answer);
+					} else if(answer.id === "display2"){
+						$("#space2").removeClass("unanswered");
+						audioCorrect(answer);
+					} else if(answer.id === "display3"){
+						$("#space3").removeClass("unanswered");
+						audioCorrect(answer);
+					} else if(answer.id === "display4"){
+						$("#space4").removeClass("unanswered");
+						audioCorrect(answer);
+					} else if(answer.id === "display5"){
+						$("#space5").removeClass("unanswered");
+						audioCorrect(answer);
+					} else {
+					// alert("uh oh, something went wrong. Please try again.");
+					}
+				};
+					// Play the chime sound unless the button has already been clicked:
+				function audioCorrect(clicked) {
+					let correctSound = document.getElementById("correctAudio");
+					correctSound.currentTime = 0;
+					console.log("value of clicked div:" + clicked.value)
+					if(clicked.value === ""){
+						correctSound.play();
+						$(clicked).val("clicked");
+					}
+				}
+
+				// Displays the text, plays the chime sound, and changes the color of the answer when clicked. Also pushes response to firebase if not previously clicked:
+				$("#display1, #display2, #display3, #display4, #display5").on("click", function(){
+					// if(this.value != "clicked"){
+					// }
+					answerClick(this);
+				});
+
+				// Will perform count answer as clicked if corresponding number on keyboard is pressed:
+				document.addEventListener("keydown", function(e){
+					let wrongSound = document.getElementById("wrongAudio");
+					wrongSound.currentTime = 0;
+					if (e.keyCode === 49 || e.keyCode === 97) {
+						answerClick(display1);
+					} else if (e.keyCode === 50 || e.keyCode ===  98) {
+						answerClick(display2);
+					} else if (e.keyCode === 51 || e.keyCode === 99) {
+						answerClick(display3);
+					} else if (e.keyCode === 52 || e.keyCode === 100) {
+						answerClick(display4);
+					} else if (e.keyCode === 53 || e.keyCode === 101) {
+						answerClick(display5);
+					} else if (e.keyCode === 32) {
+						strikeCount++;
+						if (strikeCount < 4) {
+						$("#strikeDisplay").append("<h2 id='strike'>X</h2>");
+						wrongSound.play();
+						}
+					}
+				});
+
+				// Function that sends the clicked button's info (ID) to the API:
+				// function apiSend(clicked){
+				// 	this.ref().push({
+				// 		clicked: clicked.id
+				// 	});
+				// };
+
+				// GAME HOST SCREEN:
+
+				// function questionDisplay() {
+				// 	//Runs through the length of the array, and injects this HTML and information for each question:
+				// 	for (let i = 0; i <= this.length; i++) {
+				// 		console.log(this[i].question_text);
+				// 		$("#displayZone").append('<div class="panel panel-primary"><div class="panel-heading text-center"><h4><strong>' + 
+				// 			this[i].question_text + '</strong></h4></div><div class="panel panel-body"><button class="btn btn-primary btn-block">' + 
+				// 			this[i].choiceOne + '</button><button class="btn btn-primary btn-block">' + 
+				// 			this[i].choiceTwo + '</button><button class="btn btn-primary btn-block">' + 
+				// 			this[i].choiceThree + '</button><button class="btn btn-primary btn-block">' + 
+				// 			this[i].choiceFour + '</button><button class="btn btn-primary btn-block">' + 
+				// 			this[i].choiceFive + '</button><h4>Question Number: ' + 
+				// 			this[i].id + '</h4><h4>Disclaiming information: ' + 
+				// 			this[i].disclaimers + '</div></div>');
+				// 	}
+				// }
+
+				// questionDisplay();
+
+				// this.ref().on("child_added", function(snapshot) {
+				// 	console.log(snapshot.val().clicked);
+				// 	answerClick(snapshot.val().clicked);
+				// })
+})
